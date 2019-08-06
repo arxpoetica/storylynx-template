225d7e9c9910586f57b0d04207c423e8dcf2367a
Robert Hall <thebookofrobert@gmail.com>
2019-08-06 07:16:23 -0600

    Asset card

--- src/routes/assets/_components/Asset.svelte
+++ src/routes/assets/_components/Asset.svelte
@@ -1,34 +1,42 @@
-<!-- <p>{JSON.stringify(resource)}</p> -->
+<!-- <p>{JSON.stringify(asset)}</p> -->
 
-<div class="resource">
-	<a href="/assets/{resource.id}" rel=prefetch>
+<div class="asset">
+	<a href="/assets/{asset.slug}" rel=prefetch>
 		<LazyImg {src} {alt}/>
-		<h2>{resource.title || '[This resource has no title.]'}</h2>
-		<h3>{formattedstamp(resource.createdAt)}</h3>
+		<h2>{asset.title || '[This Asset has no title.]'}</h2>
+		<h3>{formattedstamp(asset.publishedDatetime)}</h3>
 	</a>
-	<p>{summary}</p>
+	<!-- <p>externalAssets: {asset.externalAssets}</p> -->
+	<p>{asset.summary}</p>
 	<h4>Tags:</h4>
 	<div class="tags">
-		{#each resource.tags as tag}
+		{#each asset.tags.map(tag => tag.tag) as tag}
 			<strong>{tag}</strong>
 		{/each}
 	</div>
 </div>
 
 <script>
-	import { formattedstamp } from '@johnny/utils/basic-utils'
-	import LazyImg from '../../components/shared/LazyImg.svelte'
-	export let resource
+	import { src as source, formattedstamp } from '@johnny/utils/basic-utils.js'
+	import LazyImg from '../../../components/shared/LazyImg.svelte'
+	export let asset
 
+	// {#each item.assets as img}
+	// 	<img src={img.url} alt={img.summary || 'nada'}/>
+	// 	<!-- assets { id url summary handle mimeType filename } -->
+	// {/each}
 	// FIXME: have a better backup asset
-	$: src = resource.thumb ? resource.thumb : 'https://wearehubgames-website.s3.amazonaws.com/hero/upload/775/logo-blank-big.svg'
-	$: alt = resource.summary ? resource.summary : 'blank'
+	$: src =
+		asset.assets.length
+		? source(asset.assets[0], { crop: true })
+		: 'https://wearehubgames-website.s3.amazonaws.com/hero/upload/775/logo-blank-big.svg'
+	$: alt = asset.summary ? asset.summary : 'No description for this asset.'
 
 	let summary = ''
-	$: if (resource.summary) {
-		summary = resource.summary
-	// } else if (resource.content.text) { // FIXME: change content type to RichTextAST:
-	// 	const text = resource.content.text
+	$: if (asset.summary) {
+		summary = asset.summary
+	// } else if (asset.content.text) { // FIXME: change content type to RichTextAST:
+	// 	const text = asset.content.text
 	// 	summary = text.length > 100 ? text.slice(0, 99).trim() + '...' : text
 	} else {
 		summary = 'No summary.'
@@ -36,7 +44,7 @@
 </script>
 
 <style type="text/scss">
-	.resource {
+	.asset {
 		flex: 1;
 		max-width: 400rem;
 		min-width: 300rem;
--- src/routes/assets/index.svelte
+++ src/routes/assets/index.svelte
@@ -3,28 +3,9 @@
 <h1>Assets</h1>
 {#if items.length}
 	<div class="assets">
-		{#each items as item}
-			<div class="asset">
-				{item.id}
-
-				<p>id: {item.id}</p>
-				<p>publishedDatetime: {item.publishedDatetime}</p>
-				<p>title: {item.title}</p>
-				<p>slug: {item.slug}</p>
-				<p>summary: {item.summary}</p>
-				{#each item.assets as img}
-					<img src={img.url} alt={img.summary || 'nada'}/>
-					<!-- assets { id url summary handle mimeType filename } -->
-				{/each}
-				<p>externalAssets: {item.externalAssets}</p>
-				<p>
-					tags: {item.tags.map(tag => tag.tag).join(', ')}
-				</p>
-			</div>
+		{#each items as asset}
+			<Asset {asset}/>
 		{/each}
-		<!-- {#each formattedResources as resource} -->
-			<!-- <Asset {resource}/> -->
-		<!-- {/each} -->
 	</div>
 {:else}
 	<h2>Loading . . .</h2>
@@ -45,42 +26,18 @@
 	import { onMount } from 'svelte'
 	import { stores } from '@sapper/app'
 	const { page: pageStore } = stores()
-	// import { extractThumb } from './_asset-helpers'
+	import Asset from './_components/Asset.svelte'
 
-	// import Asset from './_asset.svelte'
 	export let items = []
 	export let itemsCount = 0
 	export let pageSize = 0
 	$: page = parseInt($pageStore.query.page)
-
-
-	// export let count
-	// export let resources = []
-	// let formattedResources
-	// $: {
-	// 	formattedResources = resources.map(resource => {
-	// 		return {
-	// 			id: resource.id,
-	// 			title: resource.title,
-	// 			createdAt: resource.createdAt,
-	// 			thumb: extractThumb(resource),
-	// 			summary: resource.summary,
-	// 			tags: resource.tags.map(group => group.tag)
-	// 		}
-	// 	})
-	// }
-	// let resources, pageNumber = 0, pageSize = 20;
-	// $: resources = sorted.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
 </script>
 
 <style type="text/scss">
 	.assets {
-		// display: flex;
-		// flex-wrap: wrap;
+		display: flex;
+		flex-wrap: wrap;
 		margin: 0 -6rem 20rem;
-	}
-	.asset {
-		margin: 0 0 20rem;
-		border: 1px solid gray;
 	}
 </style>
