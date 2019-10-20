87d4d5c761c76045a02a4ae40c1bdfee21867b46
Robert Hall <thebookofrobert@gmail.com>
2019-10-19 21:39:26 -0600

    Some basic work & styles on the archive lander

--- src/routes/archive/[slug].svelte
+++ src/routes/archive/[slug].svelte
@@ -4,17 +4,30 @@
 
 <!-- {JSON.stringify(asset)} -->
 
-<h1>{asset.title}</h1>
-<h2>{formattedstamp(asset.createdAt)}</h2>
+<div class="layout-main">
+	<div class="pre-texts">
+		<h1>
+			{asset.title}
+			{#if asset.decade}
+				<span>({asset.decade})</span>
+			{/if}
+		</h1>
+		<h2 class="h6">Archive | {asset.contentType || 'Uncategorized'}</h2>
+		<!-- <h2>{formattedstamp(asset.createdAt)}</h2> -->
+	</div>
+	<div class="img">
+		<LazyImg {src} {alt}/>
+	</div>
+	<div class="content">
+		{@html asset.html}
+	</div>
+</div>
+
 <h4>Tags:</h4>
 <div class="tags">
 	{#each tags as tag, index}
 		<strong>{tag}</strong>
 	{/each}
-</div>
-
-<div class="content">
-	{@html asset.html}
 </div>
 
 <script context="module">
@@ -26,9 +39,25 @@
 </script>
 
 <script>
-	import { formattedstamp } from '@johnny/utils/basic-utils'
+	import { src as source, formattedstamp } from '@johnny/utils/basic-utils'
+	import LazyImg from '@johnny/svelte/LazyImg.svelte'
+
 	export let asset
+	$: src = source(asset.assets.length ? asset.assets[0] : null, { crop: true })
+	$: alt = asset.summary ? asset.summary : 'No description for this asset.'
 	$: tags = asset.tags.map(tag => tag.tag)
 </script>
 
-<!-- <style type="text/scss"></style> -->
+<style type="text/scss">
+	.pre-texts {
+		display: flex;
+		flex-direction: column-reverse;
+		align-items: center;
+		text-align: center;
+	}
+	h1 span { display: block; }
+	h2 {
+		margin: 0 0 15rem;
+		color: $red-main;
+	}
+</style>
