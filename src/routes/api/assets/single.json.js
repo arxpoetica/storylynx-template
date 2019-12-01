1cf82cb54e7e5fe12bce653293729c6d71045642
Robert Hall <thebookofrobert@gmail.com>
2019-12-01 16:02:37 -0700

    Archive files

---
+++ src/routes/api/assets/single.json.js
@@ -0,0 +1,31 @@
+import { cmsQuery } from '@lynx/utils/loaders'
+
+export async function post(req, res) {
+	try {
+		const { resource } = await cmsQuery(`{
+			resource(where: { slug: "${req.body.slug}" }) {
+				id
+				publishedDatetime
+				title
+				summary
+				detail { html }
+				assets { id url summary handle fileName width height mimeType }
+				assetLinks {
+					summary
+					link
+					cover { url handle }
+				}
+				tags { tag }
+				contentType
+				year
+				subject
+				source
+			}
+		}`)
+		// console.log(resource)
+		return res.json(resource)
+	} catch (error) {
+		// console.log(error)
+		return res.json({ error: 1, message: error.message })
+	}
+}
