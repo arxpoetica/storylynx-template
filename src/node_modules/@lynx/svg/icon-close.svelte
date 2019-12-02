37960bb85f59ff112cf334a64c0294f47ac5d483
Robert Hall <thebookofrobert@gmail.com>
2019-12-01 17:27:15 -0700

    SVG icons

---
+++ src/node_modules/@lynx/svg/icon-close.svelte
@@ -0,0 +1,6 @@
+<svg viewBox="0 0 21 21" fill="none">
+	<path fill-rule="evenodd" clip-rule="evenodd" d="M1.482 2.793l1.311-1.31L19.482 18.17l-1.311 1.311L1.48 2.793z" fill="#000"/>
+	<path fill-rule="evenodd" clip-rule="evenodd" d="M19.482 2.793l-1.311-1.31L1.48 18.17l1.312 1.311L19.482 2.793z" fill="#000"/>
+	<path clip-rule="evenodd" d="M1.482 2.793l1.311-1.31L19.482 18.17l-1.311 1.311L1.48 2.793z" stroke="#000"/>
+	<path clip-rule="evenodd" d="M19.482 2.793l-1.311-1.31L1.48 18.17l1.312 1.311L19.482 2.793z" stroke="#000"/>
+</svg>
