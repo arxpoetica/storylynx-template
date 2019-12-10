231079ae59d2c34d2e4c5604f80d6d2a78243276
Robert Hall <thebookofrobert@gmail.com>
2019-12-09 21:46:13 -0700

    Moved API to engine

--- src/routes/api/articles/single.json.js
+++ src/routes/api/articles/single.json.js
@@ -1,23 +1,2 @@
-import { cmsQuery } from 'storylynx/utils/loaders'
-
-export async function post(req, res) {
-	try {
-		const { article } = await cmsQuery(`{
-			article(where: { slug: "${req.body.slug}" }) {
-				slug
-				headline
-				subheadline
-				byline
-				publishedDatetime
-				detail { html }
-				assets { id url summary handle fileName width height }
-				tags { tag }
-				linkBack
-			}
-		}`)
-		return res.json(article)
-	} catch (error) {
-		// console.log(error)
-		return res.json({ error: 1, message: error.message })
-	}
-}
+import { single } from 'storylynx/api/articles/single.js'
+export const post = single
