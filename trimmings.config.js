dafe32e83a13bcf9d8e78a3a2fc2eaa589b56ad4
Robert Hall <thebookofrobert@gmail.com>
2019-12-18 21:33:41 -0700

    src is already inside src

--- trimmings.config.js
+++ trimmings.config.js
@@ -1,10 +1,10 @@
 export default {
 	postcss: {
-		input: `src/node_modules/@themes/${process.env.LYNX_TEMPLATE}/src/postcss`,
+		input: `src/node_modules/@themes/${process.env.LYNX_TEMPLATE}/postcss`,
 		outputStatic: `src/node_modules/@themes/${process.env.LYNX_TEMPLATE}/static/css`,
 	},
 	svgo: {
-		input: `src/node_modules/@themes/${process.env.LYNX_TEMPLATE}/src/svgo`,
+		input: `src/node_modules/@themes/${process.env.LYNX_TEMPLATE}/svgo`,
 		outputSvelte: `src/node_modules/@themes/${process.env.LYNX_TEMPLATE}/svelte/svg`,
 		outputStatic: false,
 	},
