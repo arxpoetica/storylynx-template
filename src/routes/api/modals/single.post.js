7ef2a11047548f97c25913dec0a8837900a4c96f
Robert Hall <langbert@gmail.com>
2020-02-01 15:49:22 -0700

    Stories / modals

---
+++ src/routes/api/modals/single.post.js
@@ -0,0 +1,2 @@
+import single from 'storylynx/api/modals/single.post.js'
+export const post = single
---
+++ src/routes/stories/[name].svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>Stories | {process.env.LYNX_SITE_NAME}</title>
+</svelte:head>
+
+<NameLayout {modal}></NameLayout>
+
+<script context="module">
+	import { preloader } from 'storylynx/svelte/modals/NameLayout.preload.js'
+	export const preload = preloader
+</script>
+
+<script>
+	import NameLayout from 'storylynx/svelte/modals/NameLayout.svelte'
+	export let modal
+</script>
