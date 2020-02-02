0f72aa6b2abf4a84ae4cb9e934466fbb8cbd02b1
Robert Hall <langbert@gmail.com>
2020-02-01 20:33:05 -0700

    Stories layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,18 @@
-<MainLayout>
-	<slot></slot>
-</MainLayout>
+{#if $page.path.indexOf('/stories') === 0}
+	<LayoutStories>
+		<slot></slot>
+	</LayoutStories>
+{:else}
+	<LayoutMain>
+		<slot></slot>
+	</LayoutMain>
+{/if}
 
 <script>
 	import { setContext } from 'svelte'
 	import { stores } from '@sapper/app'
 	const { session, page, goto } = stores()
 	setContext('@sapper/app', { get_sapper_stores: () => ({ session, page, goto }) })
-	import MainLayout from 'storylynx/svelte/layout/MainLayout.svelte'
+	import LayoutMain from 'storylynx/svelte/layout/LayoutMain.svelte'
+	import LayoutStories from 'storylynx/svelte/layout/LayoutStories.svelte'
 </script>
