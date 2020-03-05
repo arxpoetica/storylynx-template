1acdfade978ff521c617c406ed7b59ef461d5790
Robert Hall <langbert@gmail.com>
2020-03-04 20:15:18 -0700

    Word of warning

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -9,10 +9,13 @@
 {/if}
 
 <script>
+	// LEAVE BELOW AS IS -- REMOVING WILL BREAK THE APP
 	import { setContext } from 'svelte'
 	import { stores } from '@sapper/app'
 	const { session, page, goto } = stores()
 	setContext('@sapper/app', { get_sapper_stores: () => ({ session, page, goto }) })
+	// LEAVE ABOVE AS IS -- REMOVING WILL BREAK THE APP
+
 	import LayoutMain from 'storylynx/svelte/layout/LayoutMain.svelte'
 	import LayoutStories from 'storylynx/svelte/stories/LayoutStories.svelte'
 </script>
