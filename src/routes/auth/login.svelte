88e9cda6a5cb0652f9ea70861cea41d3991702d3
Robert Hall <langbert@gmail.com>
2020-03-06 21:27:34 -0700

    Basic auth

---
+++ src/routes/auth/login.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Login | {process.env.LYNX_SITE_NAME}</title>
+</svelte:head>
+
+<LoginLayout/>
+
+<script context="module">
+	import { if_user_redirect } from 'storylynx/utils/preload-utils.js'
+	export const preload = if_user_redirect
+</script>
+
+<script>
+	import LoginLayout from 'storylynx/svelte/auth/LoginLayout.svelte'
+</script>
+
+<!-- <style type="text/scss"></style> -->
