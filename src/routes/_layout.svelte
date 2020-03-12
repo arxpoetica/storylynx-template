45612cdd5ae9f2ff38880de3cd7848afa92cb3fd
Robert Hall <langbert@gmail.com>
2020-03-11 22:53:55 -0600

    Starting in on admin

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,16 @@
+<!-- DEV ONLY -->
+<!-- {#if Refresh}
+	<svelte:component this={Refresh}/>
+{/if} -->
+
 {#if $page.path.indexOf('/stories') === 0}
 	<LayoutStories>
 		<slot></slot>
 	</LayoutStories>
+{:else if $page.path.indexOf('/admin') === 0}
+	<LayoutAdmin>
+		<slot></slot>
+	</LayoutAdmin>
 {:else}
 	<LayoutMain>
 		<slot></slot>
@@ -10,7 +19,7 @@
 
 <script>
 	// LEAVE BELOW AS IS -- REMOVING WILL BREAK THE APP
-	import { setContext } from 'svelte'
+	import { onMount, setContext } from 'svelte'
 	import { stores } from '@sapper/app'
 	const { session, page, goto } = stores()
 	setContext('@sapper/app', { get_sapper_stores: () => ({ session, page, goto }) })
@@ -18,4 +27,14 @@
 
 	import LayoutMain from 'storylynx/svelte/layout/LayoutMain.svelte'
 	import LayoutStories from 'storylynx/svelte/stories/LayoutStories.svelte'
+	import LayoutAdmin from 'storylynx/svelte/admin/LayoutAdmin.svelte'
+
+	// let Refresh = false
+	// onMount(async () => {
+	// 	if (process.env.NODE_ENV === 'development') {
+	// 		Refresh = (await import('./_refresh.svelte')).default
+	// 	}
+	// })
 </script>
+
+<!-- <style type="text/scss"></style> -->
---
+++ src/routes/admin/index.svelte
@@ -0,0 +1,6 @@
+<!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
+	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
+<script context="module">
+	import { if_no_user_redirect } from 'storylynx/utils/preload-utils.js'
+	export const preload = if_no_user_redirect
+</script>
