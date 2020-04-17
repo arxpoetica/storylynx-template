1391f5681b1e40a6b2f59ad54549b9c2c5553079
Robert Hall <langbert@gmail.com>
2020-04-17 17:50:58 -0600

    Moved to right page

--- src/routes/stories/[story]/[sequence].svelte
+++ src/routes/stories/[story]/[sequence].svelte
@@ -2,14 +2,14 @@
 	<title>Stories | {process.env.LYNX_SITE_NAME}</title>
 </svelte:head>
 
-<NameLayout {story}></NameLayout>
+<StoryPage {story}/>
 
 <script context="module">
-	import { preloader } from 'storylynx/svelte/stories/NameLayout.preload.js'
+	import { preloader } from 'storylynx/svelte/stories/StoryPage.preload.js'
 	export const preload = preloader
 </script>
 
 <script>
-	import NameLayout from 'storylynx/svelte/stories/NameLayout.svelte'
+	import StoryPage from 'storylynx/svelte/stories/StoryPage.svelte'
 	export let story
 </script>
