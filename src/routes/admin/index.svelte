6ba9d544c83a0ef1ae27732a79f5db0f6a46a134
Robert Hall <langbert@gmail.com>
2020-05-10 09:59:00 -0600

    Slightly better landing page

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -1,1 +1,17 @@
-<h1>Admin</h1>
+<!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
+	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
+
+<svelte:head>
+	<title>Home | Admin | {process.env.LYNX_SITE_NAME}</title>
+</svelte:head>
+
+<AdminLayout/>
+
+<!-- <script context="module">
+	import { preloader } from 'storylynx/svelte/admin/AdminLayout.preload.js'
+	export const preload = preloader
+</script> -->
+
+<script>
+	import AdminLayout from 'storylynx/svelte/admin/AdminLayout.svelte'
+</script>
