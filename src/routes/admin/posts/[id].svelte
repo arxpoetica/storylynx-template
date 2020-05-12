705f6ab8294cd5a2279ed115f9ac2b0c9fb1ed4a
Robert Hall <langbert@gmail.com>
2020-05-11 19:06:10 -0600

    Tiered layout for content panes

--- src/routes/admin/posts/[id].svelte
+++ src/routes/admin/posts/[id].svelte
@@ -1,6 +1,27 @@
 <!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
 	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
 
+<SinglePostLayout {post} {tags}/>
+
+<script context="module">
+	import { preloader } from 'storylynx/svelte/admin/posts/SinglePostLayout.preload.js'
+	export const preload = preloader
+</script>
+
+<script>
+	import SinglePostLayout from 'storylynx/svelte/admin/posts/SinglePostLayout.svelte'
+	export let post
+	export let tags
+</script>
+
+
+
+
+
+
+
+
+
 <!-- {#if copy}
 	<div class="admin-header">
 		<h1>{$page.params.id === 'new' ? 'Create' : 'Edit'} Post</h1>
---
+++ src/routes/admin/users/_layout.svelte
@@ -0,0 +1,15 @@
+<!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
+	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
+
+<LayoutContent model="Users" {...$$props}>
+	<slot></slot>
+</LayoutContent>
+
+<script context="module">
+	import { preloader } from 'storylynx/svelte/admin/users/UsersLayout.preload.js'
+	export const preload = preloader
+</script>
+
+<script>
+	import LayoutContent from 'storylynx/svelte/admin/components/layout/LayoutContent.svelte'
+</script>
