f560d219c635923d10f071abcb6764096e28488d
Robert Hall <langbert@gmail.com>
2020-05-17 13:02:10 -0600

    Quick entry & arrange

---
+++ src/routes/api/admin/assets/page-quickarrange.post.js
@@ -0,0 +1,2 @@
+import page from 'storylynx/api/admin/assets/page-quickarrange.post.js'
+export const post = page
---
+++ src/routes/api/admin/assets/page-quickentry.post.js
@@ -0,0 +1,2 @@
+import page from 'storylynx/api/admin/assets/page-quickentry.post.js'
+export const post = page
