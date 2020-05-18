a9828189490895bf06845072721a63a74c512ecc
Robert Hall <langbert@gmail.com>
2020-05-18 13:45:39 -0600

    slug -> id

--- src/routes/assets/[id].svelte
+++ src/routes/assets/[id].svelte
@@ -2,14 +2,14 @@
 	<title>{asset_group.title} | Assets | {process.env.LYNX_SITE_NAME}</title>
 </svelte:head>
 
-<SlugLayout {asset_group}></SlugLayout>
+<IdLayout {asset_group}></IdLayout>
 
 <script context="module">
-	import { preloader } from 'storylynx/svelte/assets/SlugLayout.preload.js'
+	import { preloader } from 'storylynx/svelte/assets/IdLayout.preload.js'
 	export const preload = preloader
 </script>
 
 <script>
-	import SlugLayout from 'storylynx/svelte/assets/SlugLayout.svelte'
+	import IdLayout from 'storylynx/svelte/assets/IdLayout.svelte'
 	export let asset_group
 </script>
