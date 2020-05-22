05e89c3c08de83a13f9828ee9022f0f20f1ad6b5
Robert Hall <langbert@gmail.com>
2020-05-22 11:48:35 -0600

    Removal of old HTML cruft

--- src/routes/admin/html-blocks/index.svelte
+++
@@ -1,8 +0,0 @@
-<!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
-	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
-
-<svelte:head>
-	<title>HTML Blocks | Admin | {process.env.LYNX_SITE_NAME}</title>
-</svelte:head>
-
-<!-- <script></script> -->
--- src/routes/api/admin/stories/html-blocks-page.post.js
+++
@@ -1,5 +0,0 @@
-// DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
-// DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP
-
-import page from 'storylynx/api/admin/stories/html-blocks-page.post.js'
-export const post = page
