1cd43ca382425861149a817f04ea05490305d0df
Robert Hall <langbert@gmail.com>
2020-05-31 15:30:06 -0600

    Important auth comments

--- src/routes/api/auth/login.post.js
+++ src/routes/api/auth/login.post.js
@@ -1,2 +1,5 @@
+// DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/api/auth FOLDER --
+// DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP
+
 import login from 'storylynx/api/auth/login.post.js'
 export const post = login
--- src/routes/api/auth/logout.get.js
+++ src/routes/api/auth/logout.get.js
@@ -1,2 +1,5 @@
+// DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/api/auth FOLDER --
+// DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP
+
 import logout from 'storylynx/api/auth/logout.get.js'
 export const get = logout
