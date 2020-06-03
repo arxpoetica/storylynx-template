60cea4d615b9f49d80ac2250bf58b202f72d4568
Robert Hall <langbert@gmail.com>
2020-06-03 16:47:29 -0600

    Props fix

--- src/routes/assets/index.svelte
+++ src/routes/assets/index.svelte
@@ -1,6 +1,6 @@
 <svelte:head><title>{process.env.LYNX_SITE_NAME} - Assets</title></svelte:head>
 
-<IndexLayout {items} {items_count} {content_types} {decades} {subjects} {page_size}></IndexLayout>
+<IndexLayout {...$$props}/>
 
 <script context="module">
 	import { preloader } from 'storylynx/svelte/assets/IndexLayout.preload.js'
@@ -9,10 +9,4 @@
 
 <script>
 	import IndexLayout from 'storylynx/svelte/assets/IndexLayout.svelte'
-	export let items
-	export let items_count = 0
-	export let content_types = []
-	export let decades = []
-	export let subjects = []
-	export let page_size = 0
 </script>
--- src/routes/posts/index.svelte
+++ src/routes/posts/index.svelte
@@ -1,6 +1,6 @@
 <svelte:head><title>{process.env.LYNX_SITE_NAME} - Posts</title></svelte:head>
 
-<IndexLayout {items} {page_size} {items_count}></IndexLayout>
+<IndexLayout {...$$props}/>
 
 <script context="module">
 	import { preloader } from 'storylynx/svelte/posts/IndexLayout.preload.js'
@@ -9,7 +9,4 @@
 
 <script>
 	import IndexLayout from 'storylynx/svelte/posts/IndexLayout.svelte'
-	export let items = []
-	export let items_count = 0
-	export let page_size = 0
 </script>
