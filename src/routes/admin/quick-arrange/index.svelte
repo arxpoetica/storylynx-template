b3bfe85fdd04f3a7ea82eb4a7e37bdd9e7b6bb71
Robert Hall <langbert@gmail.com>
2020-07-07 01:06:30 -0600

    Ooops

--- src/routes/admin/quick-arrange/index.svelte
+++ src/routes/admin/quick-arrange/index.svelte
@@ -1,15 +1,15 @@
 <!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
 	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
 
-<IndexQuickArrange {...$$props}>
+<QuickArrangeIndex {...$$props}>
 	<slot></slot>
-</IndexQuickArrange>
+</QuickArrangeIndex>
 
 <script context="module">
-	import { preloader } from 'storylynx/svelte/admin/assets/IndexQuickArrange.preload.js'
+	import { preloader } from 'storylynx/svelte/admin/assets/QuickArrangeIndex.preload.js'
 	export const preload = preloader
 </script>
 
 <script>
-	import IndexQuickArrange from 'storylynx/svelte/admin/assets/IndexQuickArrange.svelte'
+	import QuickArrangeIndex from 'storylynx/svelte/admin/assets/QuickArrangeIndex.svelte'
 </script>
--- src/routes/admin/quick-entry/index.svelte
+++ src/routes/admin/quick-entry/index.svelte
@@ -1,15 +1,15 @@
 <!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin FOLDER --
 	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
 
-<IndexQuickentry {...$$props}>
+<QuickEntryIndex {...$$props}>
 	<slot></slot>
-</IndexQuickentry>
+</QuickEntryIndex>
 
 <script context="module">
-	import { preloader } from 'storylynx/svelte/admin/assets/IndexQuickentry.preload.js'
+	import { preloader } from 'storylynx/svelte/admin/assets/QuickEntryIndex.preload.js'
 	export const preload = preloader
 </script>
 
 <script>
-	import IndexQuickentry from 'storylynx/svelte/admin/assets/IndexQuickentry.svelte'
+	import QuickEntryIndex from 'storylynx/svelte/admin/assets/QuickEntryIndex.svelte'
 </script>
