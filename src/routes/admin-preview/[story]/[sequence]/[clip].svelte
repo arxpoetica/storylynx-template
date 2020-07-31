9b5a3cbd427e391e595c4bb8fdcabd93739fb3e5
Robert Hall <langbert@gmail.com>
2020-07-30 22:57:55 -0600

    Better preview for clip(s)

---
+++ src/routes/admin-preview/[story]/[sequence]/[clip].svelte
@@ -0,0 +1,14 @@
+<!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin-preview FOLDER --
+	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
+
+<Preview {story}/>
+
+<script context="module">
+	import { preloader } from 'storylynx/svelte/admin/stories/Preview.preload.js'
+	export const preload = preloader
+</script>
+
+<script>
+	import Preview from 'storylynx/svelte/admin/stories/Preview.svelte'
+	export let story
+</script>
---
+++ src/routes/admin-preview/_layout.svelte
@@ -0,0 +1,10 @@
+<!-- DO NOT DELETE THIS FILE OR ANY FILES IN THE /src/routes/admin-preview FOLDER --
+	 DOING SO WILL BREAK THE CMS / ADMIN CAPABILITIES OF YOUR APP -->
+
+<LayoutStories>
+	<slot></slot>
+</LayoutStories>
+
+<script>
+	import LayoutStories from 'storylynx/svelte/stories/layout/LayoutStories.svelte'
+</script>
