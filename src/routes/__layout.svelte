aa77334b94fa20ca4052d04ec9ba03ab494cafe3
Robert Hall <arxpoetica@gmail.com>
2021-06-13 17:52:34 -0600

    Gut stores and do them right

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,26 +6,8 @@
 	</LayoutMain>
 {/if}
 
-<script context="module">
-	// LEAVE BELOW AS IS -- REMOVING WILL BREAK THE APP
-	import { goto } from '$app/navigation'
-	import { session as app_session, page as app_page, goto as app_goto } from 'storylynx/stores/app-store.js'
-	export async function preload(page, session) {
-		app_session.set(session)
-		app_page.set(page)
-		app_goto.set(goto)
-		return
-	}
-	// LEAVE ABOVE AS IS -- REMOVING WILL BREAK THE APP
-</script>
-
 <script>
-	// LEAVE BELOW AS IS -- REMOVING WILL BREAK THE APP
 	import { page } from '$app/stores'
-	import { page as app_page_alt } from 'storylynx/stores/app-store.js'
-	page.subscribe(value => $app_page_alt = value)
-	// LEAVE ABOVE AS IS -- REMOVING WILL BREAK THE APP
-
 	import LayoutMain from 'storylynx/svelte/layout/LayoutMain.svelte'
 </script>
 
