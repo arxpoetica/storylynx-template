1e263c37b4d0daacdf0d1b52430f173107f020fb
Robert Hall <arxpoetica@gmail.com>
2021-06-20 21:26:30 -0600

    Necessary hooks for processing the cookie & session

--- src/___server.js
+++ src/___server.js
@@ -1,28 +1,10 @@
-// app.use(sirv(`src/node_modules/@themes/${process.env.LYNX_TEMPLATE}/static`, { development }))
-
-// app.use(sapper.middleware({
-// 	session: req => {
-// 		const token = getToken(req)
-// 		return {
-// 			user: token.unauthorized ? null : token,
-// 			cookie: req.cookies.lynx,
-// 		}
-// 	},
-// }))
-
-// app.listen(PORT, setTimeout(() => {
-// 	console.log()
-// 	console.log()
-// 	console.log('           `\\.      ,/\'')
-// 	console.log('            |\\\\____//|')
-// 	console.log('            )/_ `\' _\\(')
-// 	console.log('           ,\'/-`__\'-\\`\\     < StoryLynx >')
-// 	console.log('           /. (_><_) ,\\')
-// 	console.log('           ` )/`--\'\\(`\'')
-// 	console.log('             `      \'')
-// 	console.log()
-// 	console.log(green('     listening on port ') + PORT + green(' in ') + NODE_ENV + green(' mode'))
-// 	console.log()
-// 	// console.log(JSON.stringify(process.env, null, 2))
-// 	console.log()
-// }, 100))
+// console.log()
+// console.log()
+// console.log('           `\\.      ,/\'')
+// console.log('            |\\\\____//|')
+// console.log('            )/_ `\' _\\(')
+// console.log('           ,\'/-`__\'-\\`\\     < StoryLynx >')
+// console.log('           /. (_><_) ,\\')
+// console.log('           ` )/`--\'\\(`\'')
+// console.log('             `      \'')
+// console.log()
---
+++ src/hooks.js
@@ -0,0 +1,6 @@
+// DO NOT DELETE THIS FILE
+// DOING SO WILL BREAK THE SESSION / AUTHENTICATION CAPABILITIES OF YOUR APP
+
+import { handle_hook, session_hook } from 'storylynx/utils/hooks'
+export const handle = handle_hook
+export const getSession = session_hook
